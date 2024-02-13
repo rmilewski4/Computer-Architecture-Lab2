b0e06041cc092416890cf82481d6052da9be0ece
Ryan Milewski <rsmbby@umsystem.edu>
2024-02-13 09:03:35 -0600

    Add starter code to read from file

--- src/lab2.c
+++ src/lab2.c
@@ -1,6 +1,26 @@
 #include "lab2.h"
 
+//for wes to implement, filename stored in prog_file
+void load_program(char* instruction_array[]) {
+
+}
+
+char** initalize_program() {
+    char** inst_array = malloc(maxfilesize*sizeof(char*));
+    for(int i = 0; i < maxfilesize; i++ ) {
+        inst_array[i] = malloc(maxinstsize*sizeof(char));
+        memset(inst_array[i],0,maxinstsize);
+    }
+    return inst_array;
+}
 
 int main(int argc, char*argv[]) {
-    
+	if (argc < 2) {
+		printf("Error: You should provide input file.\nUsage: %s <input program> \n\n",  argv[0]);
+		exit(1);
+	}
+    strncpy(prog_file,argv[1],32);
+    char** instruction_array = initalize_program();
+    load_program(instruction_array);
+
 }
--- src/lab2.h
+++ src/lab2.h
@@ -4,4 +4,11 @@
 #include <stdint.h>
 #include <assert.h>
 
+char prog_file[32];
+int maxfilesize = 50;
+int maxinstsize = 255;
+//Read in input from prog_file and store into memory array.
+void load_program(char* instruction_array[]);
 
+//Needed to initalize array to store instructions
+char** initalize_program();
